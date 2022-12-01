bde3513c16d502306bee98089fd641fd2b713772
liuxinxin <cliuxinxin@gmail.com>
2022-12-01 08:43:00 +0800

    add symbol

--- text/symbols.py
+++ text/symbols.py
@@ -3,7 +3,7 @@
 '''
 Defines the set of symbols used in text input to the model.
 '''
-_pad        = '_'
+_pad        = '_-'
 _punctuation = ';:,.!?¡¿—…"«»“” ，。、？！：；（）《》'
 _dd = "'"
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
